970a14e2ac1674f916df6d59c9ecd140f8aecfa0
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2024-01-04 11:16:39 +0600

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,47 +1,93 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"; //this is bearer token that means whoever has this token can access the protected routes
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-  username:{
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true, //best practice for text search
-  },
-  email:{
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname:{
-    type: String,
-    required: true,
-    trim: true,
-    index: true, //best practice for text search
-  },
-  avater:{
-    type: String, //collect from cloudinary
-    required: true,
-  },
-  coverImage:{
-    type: String, //collect from cloudinary
-  },
-  watchHistory: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Video',
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, //best practice for text search
     },
-  ],
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true, //best practice for text search
+    },
+    avater: {
+      type: String, //collect from cloudinary
+      required: true,
+    },
+    coverImage: {
+      type: String, //collect from cloudinary
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  refreshToken:{
-    type: String,
-  },
-}, { timestamps: true});
+  { timestamps: true }
+);
 
-export const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); //if the password is not modified, we don't need to encrypt it
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+}); //it's a middleware where we'll encrypt the password before saving it to the database
+
+//check if the password is matched or not
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//generate token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+//generate refresh token
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+export const User = mongoose.model("User", userSchema);
